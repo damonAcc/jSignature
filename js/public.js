2c7837754f429a295fb90c197bf47dac9e7ea832
Liyi <yi.li01@bqjr.cn>
2016-12-01 11:30:14 +0800

    --

--- js/public.js
+++ js/public.js
@@ -1,15 +1,16 @@
 new function (){
-	var _self = this;
-	_self.width = 640;//设置默认最大宽度
-	_self.fontSize = 100;//默认字体大小
-	_self.widthProportion = function(){var p = (document.body&&document.body.clientWidth||document.getElementsByTagName("html")[0].offsetWidth)/_self.width;return p<0.5?0.5:p;};
-	
-	var lastProportion = _self.widthProportion()>0.75?0.75:_self.widthProportion();
-	_self.changePage = function(){
-		document.getElementsByTagName("html")[0].setAttribute("style","font-size:"+lastProportion*_self.fontSize+"px !important");
-	}
-	_self.changePage();
-	window.addEventListener("resize",function(){_self.changePage();},false);
+    var _self = this;
+    _self.width = 640;//设置默认最大宽度
+    _self.fontSize = 100;//默认字体大小
+    _self.widthProportion = function(){
+        var p = (document.body&&document.body.clientWidth||document.getElementsByTagName("html")[0].offsetWidth)/_self.width;return p<0.5?0.5:(p>0.75?0.75:p);
+    };
+    
+    _self.changePage = function(){
+        document.getElementsByTagName("html")[0].setAttribute("style","font-size:"+_self.widthProportion()*_self.fontSize+"px !important");
+    }
+    _self.changePage();
+    window.addEventListener("resize",function(){_self.changePage();},false);
 };
 
 
